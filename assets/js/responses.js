ef8a3f3817c517377efba792db7846e3398f57c9
Prasoon Ghosh <prasoonpg.ghosh@gmail.com>
2022-08-21 13:23:27 +0530

    Final Commit

--- assets/js/responses.js
+++ assets/js/responses.js
@@ -1,4 +1,8 @@
 function getBotResponse(input) {
+    //objects of four wheeler vehicles
+    var Cars= ["Maruti Brezaa","Hyundai i20","Mahindra Scorpio","Maruti Suzuki Swift","Honda City"]
+
+    
     //rock paper scissors
     if (input == "rock") {
         return "paper";
@@ -7,11 +11,34 @@ function getBotResponse(input) {
     } else if (input == "scissors") {
         return "rock";
     }
+    var category = ["Two Wheelers ", "Four Wheelers"]
 
     // Simple responses
     if (input == "hello") {
-        return "Hello there!";
-    } else if (input == "goodbye") {
+        return "Hello there! Would you like to rent your vehicle?";
+    } 
+    else if(input == "yes"){
+        return "Please enter your age."
+    }
+    else if(input == "no"){
+        return "Good bye"
+    }
+    else if(input >= 18){
+        return "You are eligible for renting your vehicle. Please enter your name"
+    }
+    else if(input <= 18){
+        return "Sorry, you are not eligible to rent a vahicle."
+    }
+    else if(input == "prasoon"){
+        return "Please choose your category:\n Two Wheelers \n Four Wheelers";
+    }
+    else if(input == "four wheelers"){
+        return "Choose your vehicle: " + Cars;
+    }
+    if(input == Cars){
+        return "Enter the date of renting"
+    }
+    else if (input == "goodbye") {
         return "Talk to you later!";
     } else {
         return "Try asking something else!";
